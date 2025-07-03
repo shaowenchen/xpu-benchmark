a8d62dd6146a4e5502fe17e0b700b413ef140c96
shaowenchen <mail@chenshaowen.com>
2025-07-03 10:55:15 +0800

    chore: remove NPU inference and training files and related workflow
    - Delete all files under npu/inference and npu/training
    - Remove .github/workflows/build-npu-images.yml
    - Cleanup project to focus on GPU frameworks only

--- npu/inference/run-container.sh
+++
@@ -1,7 +0,0 @@
-if command -v nerdctl &>/dev/null; then
-    CONTAINER_RUNTIME="nerdctl"
-else
-    CONTAINER_RUNTIME="docker"
-fi
-
-$CONTAINER_RUNTIME run -it --rm --gpus all shaowenchen/xpu-benchmark:npu-inference-latest 
